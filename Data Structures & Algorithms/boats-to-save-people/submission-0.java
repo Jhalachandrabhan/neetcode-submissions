0099154dc0e93e24ba6ef2da0e86db5887769724
Chandrabhan Singh Jhala <jhalachandrabhansingh83@gmail.com>
2026-08-04 18:40:35 +0530

    Add: boats-to-save-people - submission-0

---
+++ Data Structures & Algorithms/boats-to-save-people/submission-0.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int numRescueBoats(int[] people, int limit) {
+        Arrays.sort(people);
+
+        int start = 0;
+        int end = people.length-1;
+        int count = 0;
+        while(start<=end)
+        {
+            int sum =people[start]+people[end];
+          if(sum<=limit)
+          {
+            start++;
+            end--;
+            count ++ ;
+          }
+          if(sum>limit)
+          {
+            end--;
+            count++;
+          }
+
+        }
+        return count;
+    }
+}
